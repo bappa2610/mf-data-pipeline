9ae68ba5af204457580b783db4de464c892f7d50
bappa2610 <59230335+bappa2610@users.noreply.github.com>
2026-01-28 15:11:53 +0530

    Update build_master_file.py

--- scripts/build_master_file.py
+++ scripts/build_master_file.py
@@ -5,7 +5,7 @@ categories = {}
 
 with open("data/scheme_codes.csv", newline="", encoding="utf-8") as f:
     for r in csv.DictReader(f):
-        codes[r["SchemeCode"]] = r["SchemeName"]
+        codes[r["SchemeCode"]] = r
 
 with open("data/scheme_categories.csv", newline="", encoding="utf-8") as f:
     for r in csv.DictReader(f):
@@ -13,14 +13,28 @@ with open("data/scheme_categories.csv", newline="", encoding="utf-8") as f:
 
 with open("mf_master.csv", "w", newline="", encoding="utf-8") as f:
     writer = csv.writer(f)
-    writer.writerow(["SchemeCode", "SchemeName", "Category", "Type", "AMC"])
+    writer.writerow([
+        "SchemeCode",
+        "AMC",
+        "SchemeName",
+        "ISIN",
+        "NAV",
+        "NAVDate",
+        "Category",
+        "Type",
+        "AMC_FundHouse"
+    ])
 
-    for code, name in codes.items():
-        cat = categories.get(code, {})
+    for code, s in codes.items():
+        c = categories.get(code, {})
         writer.writerow([
             code,
-            name,
-            cat.get("Category", ""),
-            cat.get("Type", ""),
-            cat.get("AMC", "")
+            s["AMC"],
+            s["SchemeName"],
+            s["ISIN"],
+            s["NAV"],
+            s["NAVDate"],
+            c.get("Category", ""),
+            c.get("Type", ""),
+            c.get("AMC", "")
         ])
